e7ffc30b0e94a61e87d7f558632be6e80b0f780d
N0m4d <108416813+nomadstar@users.noreply.github.com>
2024-11-12 23:34:32 -0300

    SkullyMonogame
    Fotos de SkullyEditadas y sprites!

--- frontiface/gamefiles/DeathTerminal/Game1.cs
+++ frontiface/gamefiles/DeathTerminal/Game1.cs
@@ -13,7 +13,7 @@ public class Game1 : Game
     {
         _graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
-        IsMouseVisible = true;
+        IsMouseVisible = false;
     }
 
     protected override void Initialize()
@@ -42,7 +42,7 @@ public class Game1 : Game
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
+        GraphicsDevice.Clear(Color.Black);
 
         // TODO: Add your drawing code here
 
