5b9b93928550dcf8036e6f6957816058abcb98ab
Cristopher Osorio <108844036+kasorio@users.noreply.github.com>
2024-11-20 23:16:31 -0300

    modified:   compose.yaml 	new file:   sql/init-scripts/create_tables.sql 	new file:   sql/init-scripts/seed_data.sql

---
+++ sql/init-scripts/create_tables.sql
@@ -0,0 +1,20 @@
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(25),
+    email VARCHAR(25) UNIQUE,
+    Tipo_usuario BOOLEAN,
+    contraseña VARCHAR(25),
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE niveles (
+    id SERIAL PRIMARY KEY,
+    nombre_nivel VARCHAR(25),
+    dificultad INT
+);
+
+CREATE TABLE progresion (
+    usuario_id INT REFERENCES usuarios(id),
+    nivel_id INT REFERENCES niveles(id),
+    fecha_completado TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
