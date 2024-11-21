b4a4fe5a1d79274bdd9fc48932bfd6ec8e897b64
N0m4d <108416813+nomadstar@users.noreply.github.com>
2024-11-20 21:49:33 -0300

    Revert "Hello"
    This reverts commit 52923f4abfd9049c2b52d247ab2f60de8deb749d.

--- frontiface/gamefiles/DeathTerminal/obj/Debug/net8.0/DeathTerminal.AssemblyInfo.cs
+++ frontiface/gamefiles/DeathTerminal/obj/Debug/net8.0/DeathTerminal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeathTerminal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+914083f8f473267b81e7fbe50cbc37b16b757acd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1a4adc620358d4c72a3e4744390265d910df9fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeathTerminal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeathTerminal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
