2b798e1ddb43aabe1806a3ebf4b0747bd185c3fe
ry091 <r4yen987@gmail.com>
2024-12-04 18:00:46 -0300

    cambios servicio registro

--- registro_usuarios/register.dockerfile
+++ registro_usuarios/register.dockerfile
@@ -5,5 +5,5 @@ WORKDIR /usr/src/app
 COPY pluginlist.txt .
 RUN pip install --no-cache-dir -r pluginlist.txt
 COPY . .
-
+ENV PYTHONPATH=/app
 CMD [ "python", "register.py" ] 
--- registro_usuarios/register.py
+++ registro_usuarios/register.py
@@ -1,12 +1,8 @@
 import os
 import logging
 import json
-import psycopg2
 import socket
-import time
-
-from dataclasses import dataclass
-
+from bus_conf import send_to_bus_response, register_service, receive_from_bus
 # Configuración de logging
 LOG_DIR = "./logs"
 os.makedirs(LOG_DIR, exist_ok=True)
@@ -15,192 +11,76 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
-
-# Configuración del BUS
-BUS_HOST = os.getenv("BUS_HOST", "localhost")  # Dirección del bus
-BUS_PORT = int(os.getenv("BUS_PORT", 5000))    # Puerto del bus por defecto
-
-# Configuración de PostgreSQL
-DB_CONFIG = {
-    "dbname": os.getenv("POSTGRES_DB", "db"),
-    "user": os.getenv("POSTGRES_USER", "user"),
-    "password": os.getenv("POSTGRES_password", "password"),
-    "host": "db",
-    "port": 5432
-}
-
-#recibir mensaje del bus
-def receive_from_bus(sock):
-    """
-    Recibe un mensaje del bus de servicios utilizando un socket.
-    :param sock: Socket conectado al bus.
-    :return: Diccionario con la acción y el contenido del mensaje.
-    """
+pending_requests = {}
+#funcion que procesa las respuestas recibidas
+def handle_response(sock, content):
     try:
-        # Leer los primeros 5 caracteres como longitud del mensaje
-        data_length = int(sock.recv(5).decode())
-        data = sock.recv(data_length).decode()
+        status = content.get("status")
+        action = content.get("action")
+        contenido = content.get("content", {}) #sentencia sql
+        data = contenido.get("data", []) #resultado de la consulta                
 
-        action = data[:5].strip()  # Prefijo de 8 caracteres, registro
-        content = json.loads(data[5:])  # JSON del contenido
+        #recuperar datos del diccionario
+        lista_rec= pending_requests.pop(sock)
+        nombre = lista_rec.get("nombre")
+        email = lista_rec.get("email")
+        user_password = lista_rec.get("user_password")
 
-        logging.info(f"Mensaje recibido del bus: {data}")
-        print(f"Mensaje recibido del bus: {data}")
-
-        return {"action": action, "content": content}
-    except Exception as e:
-        logging.error(f"Error al recibir mensaje del bus: {e}")
-        print(f"Error al recibir mensaje del bus: {e}")
-        return None
-#registrar el servicio con el bus
-def register_service(sock, prefix):
-    try:
-        prefix = prefix[:5] #asegurar que tenga 5 caracteres
-        registration_message = f"{len('sinit' + prefix):05}sinit{prefix}"
-        print(f"Mensaje de registro enviado: {registration_message}")
-        sock.sendall(registration_message.encode())
-        logging.info(f"Registro del servicio enviado al bus: {registration_message}")
-
-        # Confirmar registro con el bus
-        response_length = int(sock.recv(5).decode())
-        response = sock.recv(response_length).decode()
-        logging.info(f"Respuesta del bus al: {response}")
-        if "OK" not in response:
-            raise Exception(f"Registro rechazado por el bus: {response}")
-        logging.info(f"Servicio registrado correctamente con prefijo: {prefix}")
-    except Exception as e:
-        logging.error(f"Error al registrar el servicio en el bus: {e}")
-        raise
-
-#enviar mensajes al bus
-def send_to_bus_response(sock, action ,content):
-    """
-    Envía una respuesta al bus desde el servidor.
-    :param sock: Socket conectado al bus.
-    :param action: Acción/prefijo de la respuesta
-    :param content: Diccionario con el contenido de la respuesta.
-    status : ok o nk
-    """
-    try:
-        # Serializar el contenido y calcular la longitud
-        message_content = json.dumps(content)
-        prefix = action[:5]
-        full_message = f"{len(prefix + message_content):05}{prefix}{message_content}"
-
-        # Enviar al bus
-        sock.sendall(full_message.encode())
-        logging.info(f"Respuesta enviada al bus: {full_message}")
-    except Exception as e:
-        logging.error(f"Error al enviar respuesta al bus: {e}")
-
-
-#escucha activa del bus y su proceso
-def listen_to_bus():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        try:
-            # Conectar al bus
-            sock.connect((BUS_HOST, BUS_PORT))
-            logging.info("Conectado al bus de servicios")
-
-            #registrar el servicio
-            register_service(sock, "regis")
-
-            while True:
-                # Recibir mensaje del bus
-                message = receive_from_bus(sock)
-                if message:
-                    action = message["action"]
-                    content = message["content"]
-
-                    # Procesar según la acción
-                    if action == "regis":
-                        handle_registro(sock, content)
-                    else:
-                        logging.warning(f"Comunicacion con otro servicio: {action}")
-        except Exception as e:
-            logging.error(f"Error al escuchar el bus: {e}")
-            print(f"Error al escuchar el bus: {e}")
-            time.sleep(5)
-
-#funcion propia del servicio
-def handle_registro(sock, content):
-    """
-    Maneja la acción de registro de usuario.
-    :param sock: Socket conectado al bus.
-    :param content: Contenido del mensaje recibido (diccionario).
-    """
-    try:
-        nombre = content.get("nombre")
-        email = content.get("email")
-        user_password = content.get("user_password")
-
-
-        # Verificar si el usuario ya existe
-        if User.check_user(nombre)==True:
-            logging.info(f"Usuario ya registrado: {nombre}")
-            send_to_bus_response(sock, "regis",{"status": "error", "message": "El usuario ya está registrado"})
-            return
-
-        # Registrar el usuario
-        if User.register_user(nombre, email, user_password):
-            logging.info(f"Usuario registrado exitosamente: {nombre}")
-            send_to_bus_response(sock, "regis", {"status": "success", "message": "Registro exitoso"})
+        #verificar si el usuario existe o no
+        if data and len(data) >0: #el usuario existe
+            logging.info(f"el usuario ya existe")
+            send_to_bus_response(sock, "regis", {"message": "Usuario ya existe"})
+        elif isinstance(data,list) and len(data)==0:#registrar el usuario
+            sql= f"INSERT INTO usuarios (nombre, email, user_password, Tipo_usuario) VALUES ('{nombre}', '{email}', '{user_password}', 'usuario')"
+            send_to_bus_response(sock, "condb", {"sql": sql})
+        elif action == "condb" and status == "OK":
+            logging.info(f"registro exitoso")
+            send_to_bus_response(sock, "regis", {"message": "Usuario registrado exitosamente"})
         else:
-            logging.error(f"Error al registrar usuario: {nombre}")
-            send_to_bus_response(sock, "regis",{"status": "error", "message": "Error al registrar usuario"})
+            logging.info(f"Error al registrar usuario")
+            send_to_bus_response(sock, "regis", {"message": "Error al registrar usuario"})
     except Exception as e:
-        logging.error(f"Error en el manejo de registro: {e}")
-        send_to_bus_response(sock, "regis",{"status": "error", "message": "Error interno del servidor"})
+        logging.error(f"Error al manejar respuesta: {e}")
+        send_to_bus_response(sock, "regis", {"message": "Error en el servidor"})
 
+def handle_registro(sock, content):
+    try:
+        contenido=content.get("content", {})
+        nombre = contenido.get("nombre")
+        email = contenido.get("email")
+        user_password = contenido.get("user_password")
+        # Agregar a la lista s
+        pending_requests[sock] = {"nombre": nombre, "email": email, "user_password": user_password}
 
+        # Construir y enviar la consulta SQL
+        sql = f"SELECT * FROM usuarios WHERE nombre = '{nombre}'"
+        send_to_bus_response(sock, "condb", {"sql": sql})
+     
+    except Exception as e:
+        print(f"Error al manejar solicitud de registro: {e}")
+        logging.error(f"Error al manejar solicitud de registro: {e}")
+        send_to_bus_response(sock, "regis", {"message": "Error interno del servidor"})
 
-class User:
-    @staticmethod
-    def check_user(nombre):
-        try:
-            conn = psycopg2.connect(**DB_CONFIG)
-            cursor = conn.cursor()
-
-            # Consulta al usuario en la base de datos por el nombre
-            cursor.execute("SELECT id FROM usuarios WHERE nombre = %s", (nombre,))
-            user = cursor.fetchone()
-            cursor.close()
-            conn.close()
-
-            # Si el usuario existe, retorna True
-            return user is not None
-        except Exception as e:
-            logging.error(f"Error en la base de datos al verificar usuario: {e}")
-            return False
-
-    @staticmethod
-    def register_user(nombre, email, user_password):
-        try:
-            conn = psycopg2.connect(**DB_CONFIG)
-            cursor = conn.cursor()
-            tipo_usuario = "usuario"
-
-            # Insertar el nuevo usuario y obtener el ID generado
-            cursor.execute(
-                """
-                INSERT INTO usuarios (nombre, email, user_password, Tipo_usuario)
-                VALUES (%s, %s, %s, %s) RETURNING id
-                """,
-                (nombre, email, user_password, tipo_usuario)
-            )
-            user_id = cursor.fetchone()[0]
-            conn.commit()
-            cursor.close()
-            conn.close()
-            return {"status": "success", "id": user_id}
-        except psycopg2.errors.UniqueViolation:
-            logging.error(f"Error: El nombre ya está registrado")
-            return {"status": "error", "message": "El nombre ya está registrado"}
-        except Exception as e:
-            logging.error(f"Error en la base de datos al registrar usuario: {e}")
-            return {"status": "error", "message": "Error interno en el servidor"}
-
-
+      
 if __name__ == "__main__":
     logging.info("Iniciando servicio de registro de usuarios...")
-    listen_to_bus()
+    print("Iniciando servicio de registro de usuarios...")
+    sock=register_service("regis")
+    try:
+        while True:
+            message = receive_from_bus(sock)
+            action = message.get("action")
+            if not message:
+                continue
+            #ver si es solicitud o respuesta
+            if "status" in message: #respuesta
+                if action != "regis":
+                    handle_response(sock,message)
+                else:
+                    logging.info(f"Respuesta a cliente")
+            elif action == "regis": #solicitud
+                handle_registro(sock,message)
+            else:
+                logging.error(f"mensaje a otro servidor")
+    except Exception as e:
+        logging.error(f"Error: {e}")
