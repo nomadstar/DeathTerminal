487b9f510ad3aa5b11f0442821a755b621ad3f8b
ry091 <r4yen987@gmail.com>
2024-12-07 02:34:52 -0300

    servicio de clientes

--- Client/client.py
+++ Client/client.py
@@ -1,101 +1,88 @@
-import socket
-import json
-import os
+import logging
+from bus_conf import send_to_bus_response,register_service, receive_from_bus
 
-# Configuración del BUS
-BUS_HOST = os.getenv("BUS_HOST", "localhost")  # Dirección del bus
-BUS_PORT = int(os.getenv("BUS_PORT", 5000))    # Puerto del bus por defecto
+def jugar(sock):
+    print("=== Jugar ===")
+    print("Falta implementar")
 
-def send_request(action, nombre, user_password, email=None):
-    request_data = {
-        "nombre": nombre,
-        "user_password": user_password
-    }
+def informacion_usuario(sock, id):
+    send_to_bus_response(sock, "infou", {"id": id})
+    res= receive_from_bus(sock)
+    print(f"Respuesta recibida: {res}")
 
-    if action == "registro":
-        if not email:
-            print("El email es obligatorio para el registro.")
-            return
-        request_data["email"] = email
-        prefix = "regis" 
-    elif action == "login":
-        prefix = "login"  
+
+
+def registro(sock):
+    print("=== Registro de usuario ===")
+    nombre = input("Ingrese su nombre: ").strip()
+    email = input("Ingrese su correo: ").strip()
+    user_password = input("Ingrese su contraseña: ").strip()
+    
+ 
+    send_to_bus(sock, "login", {"nombre": nombre, "email": email, "user_password": user_password})
+
+    # Esperar respuesta
+    message = receive_from_bus(sock)
+    print(f"Respuesta recibida: {message}")
+
+def login(sock):
+    print("=== Iniciar sesión ===")
+    nombre = input("Ingrese su nombre: ").strip()
+    user_password = input("Ingrese su contraseña: ").strip()
+    
+    # Crear el mensaje para el servicio de login
+    send_to_bus_response(sock, "login", {"nombre": nombre, "user_password": user_password})
+
+    # Esperar respuesta
+    respuesta = receive_from_bus(sock)
+    contenido = respuesta.get("content", {})
+    id= contenido.get("id")
+    action = respuesta.get("action")
+    if "status" in respuesta  and action != "clien":
+        print("inicio de sesion exitoso")
+        while True:
+            print("1. Continuar jugando (falta)")
+            print("2. Ver mi información")
+            print("3. Cerrar sesión")
+            opcion = input("Seleccione una opción: ").strip()
+            if opcion == "1":
+                print("Continuar jugando")
+                jugar(sock)
+            elif opcion == "2":
+                print("Ver mi información")
+                informacion_usuario(sock, id)
+                print("Cerrando sesión...")
+                break
+            else:
+                print("Opción inválida")
     else:
-        print("Acción no soportada")
-        return
+        print(f"Error en el inicio de sesión: {respuesta.get('error', 'Error desconocido')}")
 
-    # Crear el mensaje a enviar
-    message_content = json.dumps(request_data)
-    full_message = f"{len(prefix + message_content):05}{prefix}{message_content}"
-    print(f"Mensaje enviado: {full_message}")
-
-    try:
-        # Conectar al bus y enviar el mensaje
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            sock.connect((BUS_HOST, BUS_PORT))
-            sock.sendall(full_message.encode())
-
-            # Recibir la respuesta del bus
-            response = receive_response(sock)
-    except Exception as e:
-        print(f"Error al enviar solicitud al bus: {e}")
-
-def receive_response(sock):
-    try:
-        # Leer los primeros 5 caracteres como longitud
-        response_length =int(sock.recv(5).decode())
-
-        # Leer los datos del mensaje
-        response = sock.recv(response_length).decode()
-        # Extraer prefijo, estado(ok, nk) y contenido
-        action = response[:5]
-        status = response[5:7]
-        content_json = response[7:]
-
-        try:
-            content = json.loads(content_json)
-        except json.JSONDecodeError as e:
-            raise ValueError(f"Error al decodificar JSON: {e}")
-
-        print(f"Mensaje recibido: {response_length:05}{action} {status} {content}")
-        return {"action": action, "status": status, "content": content}
-    except Exception as e:
-        print(f"respuesta con formato inválido: {e}")
-        return None
-
-def registro():
-    """
-    Solicita los datos necesarios para registrar un usuario y envía la solicitud.
-    """
-    nombre = input("Introduce tu nombre de usuario: ").strip()
-    user_password = input("Introduce tu contraseña: ").strip()
-    email = input("Introduce tu email: ").strip()
-
-    send_request("registro", nombre, user_password, email)
-
-def login():
-    """
-    Solicita los datos necesarios para iniciar sesión y envía la solicitud.
-    """
-    nombre = input("Introduce tu nombre de usuario: ").strip()
-    user_password = input("Introduce tu contraseña: ").strip()
-
-    send_request("login", nombre, user_password)
+def handle_response(sock, message):
+    # Aquí procesarás respuestas genéricas recibidas desde otros servicios
+    print(f"Respuesta recibida: {message}")
 
 if __name__ == "__main__":
-    print("--- Cliente para Login SOA ---")
-    while True:
-        print("1. Registrarse")
-        print("2. Iniciar sesión")
-        print("3. Salir")
-        opcion = input("Seleccione una opción: ").strip()
-
-        if opcion == "1":
-            registro()
-        elif opcion == "2":
-            login()
-        elif opcion == "3":
-            print("Saliendo del cliente.")
-            break
-        else:
-            print("Opción inválida")
+    print("--- Cliente para SOA ---")
+    sock = register_service("clien") 
+    
+    try:
+        while True:
+            print("1. Registrarse")
+            print("2. Iniciar sesión")
+            print("3. Salir")
+            opcion = input("Seleccione una opción: ").strip()
+            
+            if opcion == "1":
+                registro(sock)
+            elif opcion == "2":
+                login(sock)
+            elif opcion == "3":
+                print("Saliendo del cliente...")
+                break
+            else:
+                print("Opción inválida")
+    except Exception as e:
+        logging.error(f"Error: {e}")
+    finally:
+        sock.close()
---
+++ Client/client_admin.py
@@ -0,0 +1,68 @@
+import logging
+from bus_conf import send_to_bus_response,register_service, receive_from_bus
+
+def informacion_admin(sock):
+    while True:
+        print("1. Ver información de los usuarios")
+        print("2. Eliminar usuario, segun id")
+        print("3. Ver los niveles")
+        print("4. Modificar niveles")
+        print("5. Eliminar niveles")
+        print("6. Salir")
+        opcion = input("Seleccione una opción: ").strip()
+        if opcion == "1":
+            send_to_bus_response(sock,"iadmi",{"opcion":opcion}) 
+        elif opcion == "2":
+            id_eliminar = input("Ingrese el ID del usuario a eliminar: ").strip()
+            send_to_bus_response(sock, "iadmi", {"opcion":opcion, "id": id_eliminar})
+        elif opcion == "3":
+            send_to_bus_response(sock,"iadmi",{"opcion":opcion})
+        elif opcion == "4":
+            id_modificar = input("Ingrese el ID del nivel a modificar: ").strip()
+            nivel = input("Ingrese el nuevo nivel: ").strip()
+            dificultad = input("Ingrese la nueva dificultad: ").strip()
+            send_to_bus_response(sock, "iadmi", {"opcion":opcion, "id": id_modificar, "nivel": nivel, "dificultad": dificultad})
+        elif opcion == "5":
+            id_eliminar = input("Ingrese el ID del nivel a eliminar: ").strip()
+            send_to_bus_response(sock, "iadmi", {"opcion":opcion, "id": id_eliminar})
+        elif opcion == "6":
+            break
+        else:
+            print("Opción inválida")
+        respuesta = receive_from_bus(sock)
+        print(f"Respuesta recibida: {respuesta}")
+
+def admin(sock):
+    print("=== Iniciar sesión ===")
+    nombre = input("Ingrese su nombre: ").strip()
+    user_password = input("Ingrese su contraseña: ").strip()
+    
+    send_to_bus_response(sock, "login", {"nombre": nombre, "user_password": user_password})
+
+   
+    respuesta = receive_from_bus(sock)
+    contenido = respuesta.get("content", {})
+    id= contenido.get("id")
+    action = respuesta.get("action")
+    if "status" in respuesta  and action != "clien":
+        print("inicio de sesion exitoso")
+        send_to_bus_response(sock, "permi", {"id":id})
+        res= receive_from_bus(sock)
+        cont = res.get("content", {})
+        msg= cont.get("message")
+        
+        if msg == True: #es administrador
+            informacion_admin(sock)
+        else:
+            print("No tiene permisos de administrador")
+    else:
+        print(f"Error en el inicio de sesión: {respuesta.get('error', 'Error desconocido')}")
+
+def handle_response(sock, message):
+    # Aquí procesarás respuestas genéricas recibidas desde otros servicios
+    print(f"Respuesta recibida: {message}")
+
+if __name__ == "__main__":
+    print("--- Cliente administrador para SOA ---")
+    sock = register_service("admin") 
+    admin(sock)
