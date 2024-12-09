6b71d1412946395a7767611b69bc45a3799cbb4c
ry091 <r4yen987@gmail.com>
2024-12-08 21:21:03 -0300

    cambios en el servicio de progreso rf6

--- progress_Service/progress.py
+++ progress_Service/progress.py
@@ -19,19 +19,35 @@ logging.basicConfig(
 def handle_response(sock, content):
     try:
         cont = content.get("content", {})
-        data = cont.get("data", []) #resultado de la consulta
+        data = cont.get("data", None) #resultado de la consulta
         logging.info(f"datos encontrados {data}")
+
+        if not data: #no se encontraron datos
+            logging.info(f"no se encontraron datos")
+            send_to_bus_response(sock, "progr", {"message": "El usuario no ha completado ningún nivel"})
+            return
+
+        elif isinstance(data, list): #[] select
+            for d in data:
+                nivel= d.get("nombre_nivel", None)
+                id_nivel=d.get("id", None)
+            send_to_bus_response(sock, "progr", {"message": "Nivel entregado" ,"nivel_actual": nivel, "id_nivel": id_nivel})
+        elif isinstance(data, dict): #{} update
+            if "consulta exitosa" in data:  # Registro exitoso
+                logging.info(f"Nuevo nivel desbloqueado: {data}")
+                send_to_bus_response(sock, "progr", {"message": "Nuevo nivel desbloqueado"})
+            else:
+                logging.warning(f"Formato inesperado en el diccionario: {data}")
+                send_to_bus_response(sock, "progr", {"message": "Formato inesperado en el resultado"})
+        else:
+            logging.error(f"Formato desconocido en data: {data}")
+            send_to_bus_response(sock, "progr", {"message": "Error al registrar usuario"})
         
 
         if not data: #no se encontraron datos
             logging.info(f"no se encontraron datos")
             send_to_bus_response(sock, "progr", {"message": "El usuario no ha completado ningún nivel"})
             return
-        
-        for d in data:
-            nivel= d.get("nombre_nivel", None)
-            id_nivel=d.get("id", None)
-        send_to_bus_response(sock, "progr", {"nivel_actual": nivel, "id_nivel": id_nivel})
     except Exception as e:
         logging.error(f"Error al manejar respuesta: {e}")
         send_to_bus_response(sock, "progr", {"message": "Error en el servidor"})
@@ -39,18 +55,33 @@ def handle_response(sock, content):
 
 def handle_solicitud(sock, content):
     #recibo el id del usuario
+    """
+    1. Ver el nivel actual del usuario
+    2. Actualizar el nivel actual del usuario
+    """
     try:
         contenido=content.get("content", {})
         id_user=contenido.get("id")
+        opcion=contenido.get("opcion")
+        if opcion==1: #preguntar por el nivel actual
+            sql= f"""SELECT niveles.nombre_nivel, niveles.id
+                    FROM progresion
+                    JOIN niveles ON progresion.nivel_id = niveles.id
+                    WHERE progresion.usuario_id = '{id_user}'
+                    ORDER BY progresion.fecha_completado DESC
+                    LIMIT 1;"""
+            send_to_bus_response(sock, "condb", {"sql": sql})
+        elif opcion==2: #actualizar el nivel actual
+            nivel=contenido.get("nivel")
+            sql=f"""UPDATE progresion
+                    SET estado = 'finalizado', fecha_completado = CURRENT_TIMESTAMP
+                    WHERE usuario_id = '{id_user}' AND nivel_id = '{nivel}';"""
 
-        #preguntar por el progreso del usuario
-        sql= f"""SELECT niveles.nombre_nivel, niveles.id
-                FROM progresion
-                JOIN niveles ON progresion.nivel_id = niveles.id
-                WHERE progresion.usuario_id = '{id_user}'
-                ORDER BY progresion.fecha_completado DESC
-                LIMIT 1;"""
-        send_to_bus_response(sock, "condb", {"sql": sql})
+            
+            send_to_bus_response(sock, "condb", {"sql": sql})
+        else:
+            logging.error(f"Opción no válida")
+            send_to_bus_response(sock, "progr", {"message": "Opción no válida"})
     except Exception as e:
         logging.error(f"Error al manejar solicitud de información: {e}")
         send_to_bus_response(sock, "progr", {"message": "Error en el servidor"})
