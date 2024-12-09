21eb51f923153d684e336f08b1abb7f698ff3ed7
ry091 <r4yen987@gmail.com>
2024-12-08 22:24:40 -0300

    cambios base de datos

--- sql/init-scripts/seed_data.sql
+++ sql/init-scripts/seed_data.sql
@@ -1,14 +1,20 @@
-INSERT INTO usuarios (nombre, email, user_password) VALUES
-('Admin', 'admin@example.com', 'password123'),
-('Jugador1', 'jugador1@example.com', '12345');
-
-INSERT INTO niveles (nombre_nivel, dificultad) VALUES
-('Nivel 1', 1),
-('Nivel 2', 2);
-
 INSERT INTO usuarios (nombre, email, user_password, Tipo_usuario) VALUES
-('hola', 'hola@example.com', '1234', 'admin');
+('Admin', 'admin@example.com', 'password123', NULL), 
+('Jugador1', 'jugador1@example.com', '12345', NULL),   
+('Jugador2', 'jugador@example.com', '12345', NULL),     
+('hola', 'hola@example.com', '1234', 'admin');          
 
-INSERT INTO progresion (usuario_id, nivel_id, fecha_completado) VALUES
-(1, 1, CURRENT_TIMESTAMP), -- Admin completó Nivel 1
-(2, 2, CURRENT_TIMESTAMP); -- Jugador1 completó Nivel 2
+INSERT INTO niveles (nombre_nivel, dificultad, creado_por) VALUES
+('Nivel 1', 1, 4), 
+('Nivel 2', 2, 4); 
+
+
+INSERT INTO progresion (usuario_id, nivel_id, estado, fecha_inicio, fecha_completado) VALUES
+(1, 1, 'finalizado', '2024-12-06 10:00:00', '2024-12-06 11:30:00'), 
+(2, 1, 'en_proceso', '2024-12-06 12:00:00', NULL),                   
+(3, 1, 'en_proceso', '2024-12-06 12:00:00', NULL);                   
+
+
+INSERT INTO trivias (nivel_id, pregunta, respuesta, creado_por) VALUES 
+(1, '¿Cuál es la capital de Francia?', 'París', 4),
+(1, '¿En qué año comenzó la Segunda Guerra Mundial?', '1939', 4); 
